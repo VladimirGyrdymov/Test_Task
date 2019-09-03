37c1831ed232b198a4aa9a1ec4712ea91a28ad95
vgyrdymov90@gmail.com <vgyrdymov90@gmail.com>
2019-09-03 20:55:13 +0300

    Добавьте файлы проекта.

---
+++ Test_Task/Application.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Test_Task
+{
+    partial class Application
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fileChoice = new System.Windows.Forms.OpenFileDialog();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.stopDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(838, 359);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fileChoice
+            // 
+            this.fileChoice.FileName = "openFileDialog1";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(12, 12);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(157, 23);
+            this.openFileBtn.TabIndex = 1;
+            this.openFileBtn.Text = "Выберите файл";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.Location = new System.Drawing.Point(12, 520);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(838, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // stopDownload
+            // 
+            this.stopDownload.Location = new System.Drawing.Point(693, 482);
+            this.stopDownload.Name = "stopDownload";
+            this.stopDownload.Size = new System.Drawing.Size(157, 23);
+            this.stopDownload.TabIndex = 4;
+            this.stopDownload.Text = "Отменить загрузку";
+            this.stopDownload.UseVisualStyleBackColor = true;
+            this.stopDownload.Click += new System.EventHandler(this.stopDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Прогресс загрузки";
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 555);
+            this.Controls.Add(this.stopDownload);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.Name = "Application";
+            this.Text = "Обработчик файлов";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog fileChoice;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button stopDownload;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test_Task/Application.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+
+namespace Test_Task
+{
+    public partial class Application : Form
+    {
+        public Application()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        string connectionString = @"Data Source = ВЛАДИМИР-ПК\SQLEXPRESS; Initial Catalog = FileProcess; Trusted_Connection = True";
+        string fileName, request;
+        int minCount = 0, maxCount;
+        const int FIRST_ELEMENT = 0, CORRECTION_ONE = 1, CORRECTION_TWO = 2;
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            openFileBtn.Enabled = false;
+
+            fileChoice.ShowDialog();
+            fileName = fileChoice.FileName;
+
+            backgroundWorker1.RunWorkerAsync();
+            label1.Text = "Процесс загрузки";
+
+            dataGridView1.DataSource = null;
+            stopDownload.Enabled = true;
+
+            maxCount = System.IO.File.ReadAllLines(fileName).Length;
+            progressBar1.Minimum = minCount;
+            progressBar1.Maximum = maxCount - CORRECTION_ONE;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] headLineCut, cutLines;
+            string[,] cutFileText;
+            List<string> textLines = new List<string>();
+
+            StreamReader sr = new StreamReader(fileName);
+            while (!sr.EndOfStream)
+            {
+                if (backgroundWorker1.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    textLines.Add(sr.ReadLine());
+                }
+            }
+
+            headLineCut = textLines[FIRST_ELEMENT].Split(';');
+            textLines.RemoveAt(FIRST_ELEMENT);
+            cutFileText = new string[textLines.Count, headLineCut.Length - CORRECTION_ONE];
+
+            for (int i = 0; i < textLines.Count; i++)
+            {
+                cutLines = textLines[i].Split(';');
+                for (int j = 0; j < cutLines.Length - CORRECTION_ONE; j++)
+                {
+                    cutFileText[i, j] = cutLines[j];
+                }
+            }
+
+            request = "CREATE TABLE data (id int, ";
+            for (int i = 0; i < headLineCut.Length - CORRECTION_ONE; i++)
+            {
+                request += '"' + headLineCut[i] + '"' + " nvarchar(MAX)" + ", ";
+            }
+            int lng = request.Length;
+            request = request.Remove(lng - CORRECTION_TWO);
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand cmdCreate = new SqlCommand(request + ")", conn);
+            cmdCreate.ExecuteNonQuery();
+
+            for (int i = 0; i < textLines.Count; i++)
+            {
+                if (backgroundWorker1.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    backgroundWorker1.ReportProgress(i + CORRECTION_ONE);
+
+                    SqlCommand cmdInsert = new SqlCommand("INSERT INTO data (id) VALUES (@id)", conn);
+                    cmdInsert.Parameters.AddWithValue("@id", i);
+                    cmdInsert.ExecuteNonQuery();
+
+                    for (int j = 0; j < headLineCut.Length - CORRECTION_ONE; j++)
+                    {
+                        SqlCommand cmdUpdate = new SqlCommand("UPDATE data SET " + '"' + headLineCut[j] + '"' + " = " + "'" + cutFileText[i, j] + "'" + "where id = " + i, conn);
+                        cmdUpdate.ExecuteNonQuery();
+                    }
+                }
+            }
+            conn.Close();
+
+            request = string.Empty;
+            for (int i = 0; i < headLineCut.Length - CORRECTION_ONE; i++)
+            {
+                request += '"' + headLineCut[i] + '"' + ", ";
+            }
+            lng = request.Length;
+            request = request.Remove(lng - CORRECTION_TWO);
+        }
+
+        private void stopDownload_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            if (e.Cancelled == true)
+            {
+                label1.Text = "Загрузка прервана";
+                stopDownload.Enabled = false;
+                openFileBtn.Enabled = true;
+                progressBar1.Value = progressBar1.Minimum;
+                SqlCommand cmd = new SqlCommand("DROP TABLE data", conn);
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                label1.Text = "Загрузка успешно завершена";
+                stopDownload.Enabled = false;
+                openFileBtn.Enabled = true;
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT " + request + " FROM data", conn);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                SqlCommand cmd = new SqlCommand("DROP TABLE data", conn);
+                cmd.ExecuteNonQuery();
+            }
+
+            conn.Close();
+        }
+    }
+}
